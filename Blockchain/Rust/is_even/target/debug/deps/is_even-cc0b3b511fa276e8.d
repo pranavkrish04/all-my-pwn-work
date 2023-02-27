3b5587c53c9167a013fcd42fd1c405c6f67bd745
tourpran <thepranavkrish04@gmail.com>
2023-02-27 20:42:12 +0530

    blockchain deleted

--- Blockchain/Rust/is_even/target/debug/deps/is_even-cc0b3b511fa276e8.d
+++
@@ -1,5 +0,0 @@
-/home/tourpran/Downloads/all-my-pwn-work/Blockchain/Rust/is_even/target/debug/deps/is_even-cc0b3b511fa276e8: src/main.rs
-
-/home/tourpran/Downloads/all-my-pwn-work/Blockchain/Rust/is_even/target/debug/deps/is_even-cc0b3b511fa276e8.d: src/main.rs
-
-src/main.rs:
--- Blockchain/Rust/is_even/target/debug/is_even.d
+++
@@ -1,1 +0,0 @@
-/home/tourpran/Downloads/all-my-pwn-work/Blockchain/Rust/is_even/target/debug/is_even: /home/tourpran/Downloads/all-my-pwn-work/Blockchain/Rust/is_even/src/main.rs
